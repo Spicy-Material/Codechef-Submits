05f526dd867976207c1b05694a2b7bf1a1526801
Mohit <53911304+Spicy-Material@users.noreply.github.com>
2019-10-20 10:03:31 +0530

    FSQRT

---
+++ Practice/Beginner/Finding Square Roots.c
@@ -0,0 +1,14 @@
+//Spicy-Material
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,t;
+    scanf("%d",&t);
+    while(t--)
+    {
+    scanf("%d",&a);
+    b=sqrt(a);
+    printf("%d\n",b);
+    }
+}
