64550b901a8f6dd142ae93d7fb7c9996d40387e4
Mohit <53911304+Spicy-Material@users.noreply.github.com>
2019-10-20 09:56:52 +0530

    HS08TEST

---
+++ Practice/Beginner/ATM.c
@@ -0,0 +1,23 @@
+//Spicy-Material
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+	scanf("%d",&a);
+	scanf("%f",&b);
+
+	if ((a%5==0)&&(a+0.50<=b))
+	{
+
+	    b=(b-a)-0.50;
+	    printf("%.2f",b);
+	}
+	else
+	
+	 printf("%.2f",b);
+	
+	return 0;
+}
+
